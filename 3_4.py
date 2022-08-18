21e3e6325b3937172e719b40daa24cc5c31b4693
real_stag_beetle <antoxazhuk@gmail.com>
2022-08-18 03:21:50 +0300

    Made 4th task (changed some pieces of interface text): >3_4 'Enter first number ' 'Enter second number ' 'Enter third number '

--- 3_4.py
+++ 3_4.py
@@ -1,6 +1,6 @@
-a = (float(input('Enter a ')))
-b = (float(input('Enter b ')))
-c = (float(input('Enter c ')))
+a = (float(input('Enter first number ')))
+b = (float(input('Enter second number ')))
+c = (float(input('Enter third number ')))
 x = 0
 print('Positives quantity: ', ((a > x), (b > x), (c > x)).count(True))
 print('Negative quantity: ', ((a < x), (b < x), (c < x)).count(True))
