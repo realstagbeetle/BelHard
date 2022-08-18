a0d88985f539e6abd3b06eae948dc0d6912cadd3
real_stag_beetle <antoxazhuk@gmail.com>
2022-08-18 10:31:34 +0300

    4th task revised (deleted excess brackets): >3_4

--- 3_4.py
+++ 3_4.py
@@ -1,6 +1,6 @@
-a = (float(input('Enter first number ')))
-b = (float(input('Enter second number ')))
-c = (float(input('Enter third number ')))
+a = float(input('Enter first number '))
+b = float(input('Enter second number '))
+c = float(input('Enter third number '))
 x = 0
 print('Positives quantity: ', ((a > x), (b > x), (c > x)).count(True))
 print('Negative quantity: ', ((a < x), (b < x), (c < x)).count(True))
